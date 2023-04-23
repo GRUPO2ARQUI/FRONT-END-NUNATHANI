f971b299122935df236c388e5f963bd81b7425fc
Alexandra Nicole Colichon Diaz <106614931+Nicol-c@users.noreply.github.com>
2023-04-23 00:24:51 -0500

    Update especialidad-insertar.component.ts

--- demo01/src/app/component/especialidad/especialidad-insertar/especialidad-insertar.component.ts
+++ demo01/src/app/component/especialidad/especialidad-insertar/especialidad-insertar.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Especialidad } from 'src/app/model/especialidad';
 import { EspecialidadService } from 'src/app/service/especialidad.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-especialidad-insertar',
@@ -10,14 +10,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./especialidad-insertar.component.css']
 })
 export class EspecialidadInsertarComponent implements OnInit {
+  id: number = 0;
+  edicion: boolean = false;
 
   form: FormGroup = new FormGroup({});
   especialidad: Especialidad = new Especialidad();
   mensaje: string = '';
-  constructor(private pE: EspecialidadService, private router: Router) { }
+  constructor(private eS: EspecialidadService, private router: Router, private route :ActivatedRoute) { }
 
 
   ngOnInit(): void {
+    
+    this.route.params.subscribe((data: Params) => {
+      this.id = data['id'];
+      this.edicion = data['id'] != null;
+      this.init();
+    });
+   
     this.form = new FormGroup({
 
       id: new FormControl(),
@@ -28,7 +37,7 @@ export class EspecialidadInsertarComponent implements OnInit {
 
   }
 
-  aceptar(): void {
+   aceptar(): void {
 
     this.especialidad.id = this.form.value['id'];
     this.especialidad.tipoEspecialidad = this.form.value['tipoEspecialidad'];
@@ -36,12 +45,21 @@ export class EspecialidadInsertarComponent implements OnInit {
     this.especialidad.centroEspecialidad = this.form.value['centroEspecialidad'];
 
     if (this.form.value['tipoEspecialidad'].length > 0) {
-
-      this.pE.insert(this.especialidad).subscribe((data) => {
-        this.pE.list().subscribe((data) => {
-          this.pE.setList(data);
+      if (this.edicion) {
+        //guardar lo actualizado
+        this.eS.update(this.especialidad).subscribe(() => {
+          this.eS.list().subscribe((data) => {
+            this.eS.setList(data);
+          });
         });
-      });
+       }else{
+        this.eS.insert(this.especialidad).subscribe((data) => {
+          this.eS.list().subscribe((data) => {
+            this.eS.setList(data);
+          });
+        });
+       }
+
       this.router.navigate(['especialidad']);
     } else {
 
@@ -49,12 +67,18 @@ export class EspecialidadInsertarComponent implements OnInit {
     }
 
   }
-
-
-
-
-
-
+  init() {
+    if (this.edicion) {
+      this.eS.listId(this.id).subscribe((data) => {
+        this.form = new FormGroup({
+          id: new FormControl(data.id),
+          tipoEspecialidad: new FormControl(data.tipoEspecialidad),
+          descripcionEspecialidad: new FormControl(data.descripcionEspecialidad),
+          centroEspecialidad: new FormControl(data.centroEspecialidad),
+        });
+      });
+    }
+  }
 
 
 
