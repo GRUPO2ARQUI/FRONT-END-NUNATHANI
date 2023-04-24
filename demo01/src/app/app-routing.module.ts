1131e12f1beb0cac57422b7ce151dbcaf8ea0a2a
Lorenzo26J <75453986+Lorenzo26J@users.noreply.github.com>
2023-04-23 22:01:48 -0500

    Update app-routing.module.ts

--- demo01/src/app/app-routing.module.ts
+++ demo01/src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ import { EspecialidadInsertarComponent } from './component/especialidad/especial
 import { DisponibilidadInsertarComponent } from './component/disponibilidad/disponibilidad-insertar/disponibilidad-insertar.component';
 import { DisponibilidadComponent } from './component/disponibilidad/disponibilidad.component';
 
+import { Rutinas_recreativasInsertarComponent } from './component/rutinas_recreativas/rutinas_recreativas-insertar/rutinas_recreativas-insertar.component';
+import { Rutinas_recreativasComponent } from './component/rutinas_recreativas/rutinas_recreativas.component';
+
+
 const routes: Routes = [
   {
     path: 'especialidad',
@@ -29,6 +33,16 @@ const routes: Routes = [
     children: [
       {path: 'disponibilidadinsertar',component: DisponibilidadInsertarComponent},
       {path: 'edicion/:id', component: DisponibilidadInsertarComponent},
+    ],
+  },
+  
+  {
+    path: 'rutinas_recreativas',
+    component: Rutinas_recreativasComponent
+    ,
+    children: [
+      {path: 'rutinas_recreativasinsertar', component: Rutinas_recreativasInsertarComponent},
+      {path: 'edicion/:id', component: Rutinas_recreativasInsertarComponent},
     ],
   },
   
