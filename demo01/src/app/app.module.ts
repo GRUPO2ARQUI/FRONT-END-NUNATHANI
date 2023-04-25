f2fe11a7938f892635f627ebfc884ddf1545235a
Hugo <103291989+UPCNew2002@users.noreply.github.com>
2023-04-25 11:40:09 -0500

    Update app.module.ts

--- demo01/src/app/app.module.ts
+++ demo01/src/app/app.module.ts
@@ -3,30 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpBackend, HttpClient, HttpClientModule } from '@angular/common/http';
-import { DisponibilidadComponent } from './component/disponibilidad/disponibilidad.component';
-import { DisponibilidadListarComponent } from './component/disponibilidad/disponibilidad-listar/disponibilidad-listar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HttpClientModule } from '@angular/common/http';
+import { EspecialidadListarComponent } from './component/especialidad/especialidad-listar/especialidad-listar.component';
+import { EspecialidadComponent } from './component/especialidad/especialidad.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { DisponibilidadInsertarComponent } from './component/disponibilidad/disponibilidad-insertar/disponibilidad-insertar.component'
+import { EspecialidadInsertarComponent } from './component/especialidad/especialidad-insertar/especialidad-insertar.component'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDialogModule } from '@angular/material/dialog';
-import { DisponibilidadDialogoComponent } from './component/disponibilidad/disponibilidad-listar/disponibilidad-dialogo/disponibilidad-dialogo.component';
+
+import { DisponibilidadComponent } from './component/disponibilidad/disponibilidad.component';
+import { DisponibilidadListarComponent } from './component/disponibilidad/disponibilidad-listar/disponibilidad-listar.component';
+import { DisponibilidadInsertarComponent } from './component/disponibilidad/disponibilidad-insertar/disponibilidad-insertar.component';
+
+import { Rutinas_recreativasComponent } from './component/rutinas_recreativas/rutinas_recreativas.component';
+import { Rutinas_recreativasListarComponent } from './component/rutinas_recreativas/rutinas_recreativas-listar/rutinas_recreativas-listar.component';
+import { Rutinas_recreativasInsertarComponent } from './component/rutinas_recreativas/rutinas_recreativas-insertar/rutinas_recreativas-insertar.component'
+
+import { EstadoComponent } from './component/estado/estado.component';
+import { EstadoListarComponent } from './component/estado/estado-listar/estado-listar.component';
+import { EstadoInsertarComponent } from './component/estado/estado-insertar/estado-insertar.component'
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    EspecialidadListarComponent,
+    EspecialidadComponent,
+    EspecialidadInsertarComponent,
     DisponibilidadComponent,
     DisponibilidadListarComponent,
     DisponibilidadInsertarComponent,
-    DisponibilidadDialogoComponent,
+    Rutinas_recreativasComponent,
+    Rutinas_recreativasListarComponent,
+    Rutinas_recreativasInsertarComponent,
+    EstadoComponent,
+    EstadoListarComponent,
+    EstadoInsertarComponent,
   ],
   imports: [
     BrowserModule,
@@ -38,14 +55,13 @@ import { DisponibilidadDialogoComponent } from './component/disponibilidad/dispo
     FormsModule,
     MatInputModule,
     MatSelectModule,
-    MatToolbarModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatIconModule,
     MatButtonModule,
-    MatDialogModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
